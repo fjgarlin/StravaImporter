35bb359cea9aed46b550c7053cde083b83a53897
Fran Garcia <fjgarlin@gmail.com>
2016-11-11 14:43:59 +0000

    First working version

--- index.php
+++ index.php
@@ -2,26 +2,45 @@
 
 require 'vendor/autoload.php';
 
-//********** somewhere in your routes or controllers...
-function upload($post) {
-    //process post
-    $data = null;
+use fjgarlin\StravaCsvImporter;
 
-    $config = [
-        'id' => 'CLIENT-ID',
-        'secret' => 'CLIENT-SECRET',
-        'redirect_url' => 'http://localhost:8888/'    //URL where you want to be redirected after authorizing this app
-    ];
-    $importer = new fjgarlin\StravaCsvImporter($config, $data);
-    return $importer->upload();
+// http://php.net/manual/en/function.str-getcsv.php
+function csvToArray($file_path) {
+    $csv = array_map('str_getcsv', file($file_path));
+    array_walk($csv, function(&$a) use ($csv) {
+        $a = array_combine($csv[0], $a);
+    });
+    array_shift($csv); # remove column header
+
+    return $csv;
 }
 
+//********** somewhere in your routes or controllers...
+//functionality here
+$credentials = json_decode(file_get_contents('.cred'));
+$config = [
+    'id' => $credentials->id,
+    'secret' => $credentials->secret,
+    'redirect_url' => 'http://' . $_SERVER['HTTP_HOST']
+];
+$importer = new StravaCsvImporter($config);
+
+$code = isset($_GET['code']) ? $_GET['code'] : false;
+if ($code) {
+    $importer->authorize($code);
+}
+
+$authorized = $importer->authorized();
+$authorize_url = ($authorized) ? false : $importer->getAuthorizeUrl();
+$athlete = $importer->getAthlete();
+
 $res = null;
-if (!empty($_POST)) {
-    //validate...
+if ($authorized and !empty($_POST) and !empty($_FILES)) {
+    //get parsed data
+    $csv = csvToArray($_FILES['activities']['tmp_name']);
 
     //and upload
-    $res = upload($_POST);
+    $res = $importer->upload($csv);
 }
 //********** somewhere in your routes or controllers...
 
@@ -35,56 +54,64 @@ if (!empty($_POST)) {
     </head>
     <body>
         <div class="container">
-            <h1>Strava CSV Uploader</h1>
+            <h1>
+                Strava CSV Uploader <br>
+                <small>Upload your CSV with activities to Strava.</small>
+            </h1>
             <hr>
-            <div class="row">
-                <div class="col-sm-6">
-                    <p class="lead">Upload your CSV with activities to Strava.</p>
-                    <p>
-                        Please follow the following column headers.
-                    <ul>
-                        <li><b>name</b> : Name of activity</li>
-                        <li><b>date</b> : Date (dd/mm/yyyy hh:mm)</li>
-                        <li><b>distance</b> : Distance (miles)</li>
-                        <li><b>time</b> : Time (minutes)</li>
-                    </ul>
 
-                    Then add your credentials (nothing gets stored anywhere) and attach the file and click on Submit.
-                    </p>
-                </div>
-                <div class="col-sm-6">
-                    <form method="post" action="" class="well">
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" class="form-control" id="username" required="required" name="username" placeholder="Username">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" required="required" name="password" placeholder="Password">
-                        </div>
-                        <div class="form-group">
-                            <label for="activities">File input</label>
-                            <input type="file" id="activities" required="required" name="activities">
-                            <p class="help-block">CSV containing the activities.</p>
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
-                </div>
-            </div>
+            <?php if (!$authorized): ?>
+                <p><a href="<?php echo $authorize_url; ?>">Authorize</a></p>
+            <?php else: ?>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <p>
+                            Please follow the following column headers.
+                            <ul>
+                                <li><b>name</b> : Name of activity</li>
+                                <li><b>date</b> : Date in ISO 8601 (ie: 2016-11-11T11:07:59Z)</li>
+                                <li><b>distance</b> : Distance (miles)</li>
+                                <li><b>time</b> : Time (minutes)</li>
+                            </ul>
+                            Attach the file and click on Submit.
+                        </p>
+                    </div>
+                    <div class="col-sm-6">
+                        <form method="post" action="" class="well" enctype="multipart/form-data">
+                            <input type="hidden" name="_submitted">
+                            <?php if ($athlete): ?>
+                                <div class="thumbnail text-center">
+                                    <div class="caption">
+                                        <h3><?php echo htmlspecialchars($athlete->firstname . " " . $athlete->lastname); ?></h3>
+                                        <p><?php echo htmlspecialchars($athlete->email); ?></p>
+                                        <p><a target="_blank" href="https://www.strava.com/athletes/<?php echo (int)$athlete->id; ?>" class="btn btn-primary" role="button">View profile</a></p>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
 
-            <?php if (!is_null($res)): ?>
-                <hr>
-                <?php if ($res->status): ?>
-                    <div class="alert alert-success">
-                        Activities uploaded.
+                            <div class="form-group">
+                                <label for="activities">File input</label>
+                                <input type="file" id="activities" required="required" name="activities">
+                                <p class="help-block">CSV containing the activities.</p>
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
                     </div>
-                <?php else: ?>
-                    <div class="alert alert-danger">
-                        Couldn't upload activities. <?php echo htmlspecialchars($res->message); ?>
-                    </div>
+                </div>
+
+                <?php if (!is_null($res)): ?>
+                    <hr>
+                    <?php if ($res->status): ?>
+                        <div class="alert alert-success">
+                            Activities uploaded.
+                        </div>
+                    <?php else: ?>
+                        <div class="alert alert-danger">
+                            Couldn't upload activities. <?php echo htmlspecialchars($res->message); ?>
+                        </div>
+                    <?php endif; ?>
                 <?php endif; ?>
             <?php endif; ?>
-
-        </div>
+        </div><!-- .container -->
     </body>
 </html>
--- src/StravaCsvImporter.php
+++ src/StravaCsvImporter.php
@@ -8,39 +8,97 @@ class StravaCsvImporter {
 
     private $config;
     private $data;
+    private $api;
+    private $code;
+    private $accessToken;
+    private $athlete;
 
-    public function __construct($config, $data = null)
+    public function __construct($config)
     {
         $this->config = (object)$config;
-        $this->data = $data;
+        $this->api = new StravaApi(
+            $this->config->id,
+            $this->config->secret
+        );
+
+        $this->code = null;
+        $this->data = null;
+        $this->accessToken = null;
+        $this->athlete = null;
     }
 
-    public function upload($data = null)
+    public function authorized()
     {
-        if (!is_null($data)) {
-            $this->data = $data;
+        return !is_null($this->accessToken);
+    }
+
+    public function getAuthorizeUrl()
+    {
+        return $this->api->authenticationUrl($this->config->redirect_url, 'auto', 'write', null);
+    }
+
+    public function getAthlete()
+    {
+        return $this->athlete;
+    }
+
+    public function authorize($code)
+    {
+        //set code and exchange tokens
+        $this->code = $code;
+        $accessToken = $this->api->tokenExchange($this->code);
+
+        if ($accessToken) {
+            if (isset($accessToken->athlete)) {
+                $this->athlete = $accessToken->athlete;
+            }
+            else {
+                //echo "<pre>" . print_r($accessToken, true). "</pre>"; exit;
+            }
+
+            //and finally set the accessToken
+            $this->accessToken = $accessToken->access_token;
+            $this->api->setAccessToken($this->accessToken);
+        }
+    }
+
+    public function upload($data)
+    {
+        if (!$this->authorized()) {
+            return (object)[
+                'status' => false,
+                'message' => 'Not authorized.'
+            ];
         }
 
-        if (!is_null($data)) {
-            $api = new StravaApi(
-                $this->config->id,
-                $this->config->secret
-            );
+        if (!$data or !is_array($data)) {
+            return (object)[
+                'status' => false,
+                'message' => 'Data is not valid.'
+            ];
+        }
 
-            //$api->authenticationUrl($this->config->redirect_url);
-            //$api->tokenExchange($code);
-            //$api->setAccessToken($accessToken);
-//            $api->post('activities', [
-//                'name'             => 'API Test',
-//                'type'             => 'Ride',
-//                'start_date_local' => date( 'Y-m-d\TH:i:s\Z'),
-//                'elapsed_time'     => 3600
-//            ]);
+        $this->data = $data;
+        foreach ($this->data as $activity) {
+            $activity = (object)$activity;
+
+            //http://strava.github.io/api/v3/activities/#create
+            $res = $this->api->post('activities', [
+                'name'             => $activity->name,
+                'type'             => 'Run',
+                'start_date_local' => $activity->date,                  //ISO 8601: 2016-11-11T11:07:59Z
+                'elapsed_time'     => $activity->time * 60,             //minutes to seconds
+                'distance'         => $activity->distance * 1609.34,    //miles to meters
+                'private'          => 0
+            ]);
+
+            //TODO: check if any activity was not uploaded
+            //echo "<pre>" . print_r($res, true). "</pre>"; exit;
         }
 
         return (object)[
-            'status' => false,
-            'message' => 'Unknown API error.'
+            'status' => true,
+            'message' => 'Activity uploaded'
         ];
     }
 }
